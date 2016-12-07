2c0f6d3497f1ce1f9cc4865bc4b3bdbe49ca74b0
liguohua <2818461312@qq.com>
2016-12-07 23:42:58 +0800

    first commit

---
+++ src/book/sparksql/sparksession/DatasetVsDataFrame.scala
@@ -0,0 +1,41 @@
+package book.sparksql.sparksession
+
+/**
+  * Created by liguohua on 07/12/2016.
+  */
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Logical Plans for Dataframe and Dataset
+  */
+object DatasetVsDataFrame {
+
+  case class Sales(transactionId: Int, customerId: Int, itemId: Int, amountPaid: Double)
+
+  def main(args: Array[String]) {
+
+    val sparkSession = SparkSession.builder
+      .master("local[*]")
+      .enableHiveSupport()
+      .appName(this.getClass.getName)
+      .getOrCreate()
+
+    val sparkContext = sparkSession.sparkContext
+    import sparkSession.implicits._
+
+
+    //read data from text file
+    val filePath = "hdfs://qingcheng12:9000/input/spark/sales.csv"
+    val df = sparkSession.read.option("header", "true").option("inferSchema", "true").csv(filePath)
+    val ds = sparkSession.read.option("header", "true").option("inferSchema", "true").csv(filePath).as[Sales]
+
+
+    val selectedDF = df.select("itemId")
+    val selectedDS = ds.map(_.itemId)
+
+    println(selectedDF.queryExecution.optimizedPlan.numberedTreeString)
+    println(selectedDS.queryExecution.optimizedPlan.numberedTreeString)
+  }
+
+}
